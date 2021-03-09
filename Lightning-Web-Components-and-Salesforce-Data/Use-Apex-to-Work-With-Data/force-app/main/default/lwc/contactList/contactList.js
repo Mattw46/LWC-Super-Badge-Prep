525348fb0b96a62a0ac1bcc49d2eb70516ce3720
Mattw46 <mattw76@live.com.au>
2021-03-10 08:46:39 +1100

    completed Handle Server Errors

--- Lightning-Web-Components-and-Salesforce-Data/Use-Apex-to-Work-With-Data/force-app/main/default/lwc/contactList/contactList.js
+++ Lightning-Web-Components-and-Salesforce-Data/Use-Apex-to-Work-With-Data/force-app/main/default/lwc/contactList/contactList.js
@@ -1,4 +1,5 @@
 import { LightningElement, wire } from 'lwc';
+import { reduceErrors } from 'c/ldsUtils';
 import getContacts from '@salesforce/apex/ContactController.getContacts';
 const COLUMNS = [
     { label: 'First Name', fieldName: 'FirstName', type: 'text'},
@@ -9,5 +10,9 @@ export default class ContactList extends LightningElement {
     columns = COLUMNS;
     @wire(getContacts)
     contacts;
+    get errors() {
+        return (this.contacts.error) ?
+            reduceErrors(this.contacts.error) : [];
+    }
 }
 
